a00e0317c00063fc4956b8ddb53e15e8df93629d
VessOne7 <gorloof7@gmail.com>
2024-05-16 18:12:22 +0700

    Success

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedNum) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < removedNum.length; j++){
+      if (array[i] === removedNum[j]) {
+          array.splice(i, 1);
+          i = 0;
+      }
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
