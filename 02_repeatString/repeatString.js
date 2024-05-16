a02831a01f90e6c7ed440946717344def006621e
VessOne7 <gorloof7@gmail.com>
2024-05-16 12:32:56 +0700

    Added code. Test successful

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let theString = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++){
+    theString = theString.concat(string);
+  }
+  return theString;
 };
 
 // Do not edit below this line
