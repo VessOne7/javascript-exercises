bbafdbcd7fdcd9dd05f3ffeb807116ceef41f469
VessOne7 <gorloof7@gmail.com>
2024-05-16 18:31:50 +0700

    Need to change code. Not working yet

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    if(((firstNum && lastNum) <= 0) || (typeof (firstNum || lastNum) != number)) {
+        return "ERROR"
+    } else if (firstNum < lastNum) {
+          for (let i = firstNum; i <= lastNum; i++) {
+          sum += i;
+        }
+      return sum
+    } else {
+        for (let i = lastNum; i <= firstNum; i++) {
+            sum += i;
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
