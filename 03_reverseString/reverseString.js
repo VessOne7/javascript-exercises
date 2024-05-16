87e595b81fb6a82bf04e78afe678f150c8b6c643
VessOne7 <gorloof7@gmail.com>
2024-05-16 14:23:43 +0700

    Made a function using for loop to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  //extract the last symbol from the text and add it to a new variable
+  //delete the last symbol in the text
+  let reverseText = '';
+  for (let i = 0; i <= text.length; i++){
+    reverseText += text.substr(text.length-i, 1);
+  }
+  return reverseText
 };
 
 // Do not edit below this line
