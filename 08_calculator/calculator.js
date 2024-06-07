fe8995293c36a1d7a928681e1a6bdbe9019b1a49
VessOne7 <gorloof7@gmail.com>
2024-06-07 18:02:57 +0700

    Finished 2 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+	return Math.pow(numOne, numTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for (let i = num; i > 0; i--) {
+    result = result * i;
+  } 
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let a = str.toLowerCase().split('').filter(item => {
+      return /[a-zA-z0-9]/.test(item);
+    }).reverse().join();
 
+    let b = str.toLowerCase().split('').filter(item => {
+        return /[a-zA-z0-9]/.test(item);
+      }).join();
+  
+     return b === a;
 };
 
 // Do not edit below this line
