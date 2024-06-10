05a5f3cf0b2c23d34e79c762306c71f45d14b6e5
Vess <gorloof7@gmail.com>
2024-06-10 12:49:29 +0700

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let arr = [0, 1];
+  for (let i = 2; i <= num; i++){
+    arr.push(arr[i-1] + arr[i-2]);
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+  return arr[num];
 };
 
 // Do not edit below this line
