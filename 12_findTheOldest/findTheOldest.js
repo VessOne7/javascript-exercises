1ba3153bec913a7dafb9aea4e7cf77c1aa6e43e3
Vess <gorloof7@gmail.com>
2024-06-10 17:18:03 +0700

    Finished findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   let oldest = {
+    yearOfBirth: 0,
+    yearOfDeath: 0
+   };
+   let currentYear = new Date();
+   people.forEach(function(item) {
+    if ((item.yearOfDeath > 1) && (oldest.yearOfDeath >= 0)) {
+        if ((item.yearOfDeath - item.yearOfBirth) > ((oldest.yearOfDeath - oldest.yearOfBirth))) {
+          oldest = item;
+      }
+      } else if (oldest.yearOfDeath == undefined){
+        if ((item.yearOfDeath - item.yearOfBirth) > ((currentYear.getFullYear() - oldest.yearOfBirth))) {
+            oldest = item;
+        }
+      } else if ((currentYear.getFullYear() - item.yearOfBirth) > ((oldest.yearOfDeath - oldest.yearOfBirth))){
+        oldest = item;
+    }
+   })
+   return oldest;
 };
 
 // Do not edit below this line
